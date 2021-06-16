eb1e96da3b193c65d14b5ebbf50b6d6357bc40c4
zishanjawed <zishanexplore@gmail.com>
2021-06-16 22:30:40 +0530

    Pepcoding --> Pattern18

--- Pepcoding/Pattern/Pattern17.java
+++ Pepcoding/Pattern/Pattern17.java
@@ -1,10 +1,16 @@
 /* 
 1. You are given a number n.
-2. You've to write code to print the pattern given in output format below.
+      2. You've to write code to print the pattern given in output format below
+                                  
+                                 
+                                
+                               
+                               
+                               
 Input Format
 A number n
 Output Format
-pat171
+pat181
 Question Video
 
 
@@ -12,13 +18,15 @@ Constraints
 1 <= n <= 10
  Also, n is odd.
 Sample Input
-5
+7
 Sample Output
-		*	
-		*	*	
-*	*	*	*	*	
-		*	*	
-		*	
+*	*	*	*	*	*	*	
+	*				*	
+		*		*	
+			*	
+		*	*	*	
+	*	*	*	*	*	
+*	*	*	*	*	*	*	
 
 
 */
@@ -26,31 +34,36 @@ class Solution {
     public void printPattern(int n) {
       
        
-        int star = 1;
-        int space = n/2;
-        
-        for (int i=1; i<=n; i++){
+        int star = n;
+     int space = 0;
+     
+     for (int i = 1; i<=n; i++){
+         
+         for(int j = 1; j<=space;j++){
+            System.out.print("\t"); 
+         }
+         
+         for(int j=1;j<=star;j++){
+             if (i > 1 && i<=n/2 && j >1 && j <star){
+                 System.out.print("\t");
+             }
+             else{
+                System.out.print("*\t"); 
+             }
             
-            for(int j=1; j<=space;j++){
-                if(i == n/2+1){
-                    System.out.print("*\t");
-                }
-                else{
-                    System.out.print("\t");
-                } 
-            }
-            for(int k = 1; k<=star;k++){
-                System.out.print("*\t");
-            }
-            
-            if(i > n/2){
-                star--;
-            }else{
-                star++;
-            }
-            
-            System.out.println();
-        }
+             
+         }
+         
+         if(i > n/2){
+             space--;
+             star +=2;
+         }else{
+             space++;
+             star -=2;
+         }
+         System.out.println();
+         
+     }
 }
 
 
