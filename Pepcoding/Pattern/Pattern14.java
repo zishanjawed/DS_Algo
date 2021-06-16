2129ab28ba93aa0a430e5a9d16e9a135b6fd4324
zishanjawed <zishanexplore@gmail.com>
2021-06-16 22:19:52 +0530

    Pepcoding --> Pattern14

--- Pepcoding/Pattern/Pattern14.java
+++ Pepcoding/Pattern/Pattern14.java
@@ -1,43 +1,37 @@
 /* 
 1. You are given a number n.
-2. You've to create a pattern as shown in output format
-                                 
-                                
-                               
+2. You've to write code to print it's multiplication table up to 10 in format given below.
 Input Format
-A number n
+A number x
 Output Format
-pat131
+pat141
 Question Video
 
 
 Constraints
 1 <= n <= 10
 Sample Input
-5
+3
 Sample Output
-1	
-1	1	
-1	2	1	
-1	3	3	1	
-1	4	6	4	1	
+3 * 1 = 3
+3 * 2 = 6
+3 * 3 = 9
+3 * 4 = 12
+3 * 5 = 15
+3 * 6 = 18
+3 * 7 = 21
+3 * 8 = 24
+3 * 9 = 27
+3 * 10 = 30
+	
 
 */
 class Solution {
     public void printPattern(int n) {
       
        
-        for (int i = 0; i<n; i++){
-            
-            int val = 1;
-            for(int j = 0; j <= i; j++){
-                System.out.print(val+"\t");
-                val = (val * (i - j))/(j+1);
-                
-            }
-            System.out.println();
-            
-            
+        for (int i = 1;i<=10;i++){
+            System.out.println(n+" * "+i+" = "+(n*i));
             
         }
 
