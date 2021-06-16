4f87766f45d7fa5e05bc6a7e48adbf7177e448bc
zishanjawed <zishanexplore@gmail.com>
2021-06-16 19:54:20 +0530

    Pepcoding --> Pattern3

---
+++ Pepcoding/Pattern/Pattern3.java
@@ -0,0 +1,59 @@
+/* 
+
+1. You are given a number n.
+2. You've to create a pattern of * and separated by tab as shown in output format.
+Input Format
+A number n
+Output Format
+pat31
+Question Video
+
+
+Constraints
+1 <= n <= 10
+Sample Input
+5
+Sample Output
+				*	
+			*	*	
+		*	*	*	
+	*	*	*	*	
+*	*	*	*	*	
+
+
+*/
+class Solution {
+    public void printPattern1(int n) {
+      
+        for (int i=1; i<=n; i++){
+            
+            for (int j = 1; j<=n; j++){
+                if(j > n - i)
+                {
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+            
+            System.out.println();
+        }
+        
+    }
+}
+
+
+class Pattern1{
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        int n = 20;
+        
+        
+       obj.printPattern1(n);
+
+        
+        
+    }
+}
