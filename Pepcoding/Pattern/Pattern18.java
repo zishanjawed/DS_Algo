1da2beab41719bf11eb1b6ab91e06ceaafafba0f
zishanjawed <zishanexplore@gmail.com>
2021-06-16 22:31:58 +0530

    Pepcoding --> Pattern19

---
+++ Pepcoding/Pattern/Pattern18.java
@@ -0,0 +1,73 @@
+/* 
+1. You are given a number n.
+      2. You've to write code to print the pattern given in output format below
+                                  
+                                 
+                                
+                               
+                               
+                               
+Input Format
+A number n
+Output Format
+pat191
+Question Video
+
+
+Constraints
+1 <= n <= 10
+ Also, n is odd.
+Sample Input
+3
+Sample Output
+*	*	*	
+*	*	*	
+*	*	*	
+*/
+class Solution {
+    public void printPattern(int n) {
+      
+       
+        for (int i=1; i<=n;i++){
+            for(int j=1; j<=n;j++){
+                
+                if(i == n/2+1 || j == n/2+1){
+                    System.out.print("*\t");
+                }
+                else if(i ==1 && j <= n/2){
+                   System.out.print("*\t"); 
+                }
+                else if(j == 1 &&  i >= n/2+1 ){
+                   System.out.print("*\t");  
+                }
+                else if(j == n &&  i <= n/2 ){
+                   System.out.print("*\t");  
+                }
+                else if(i == n &&  j >= n/2+1 ){
+                   System.out.print("*\t");  
+                }
+                else{
+                   System.out.print("\t"); 
+                }
+                
+               
+            }
+            System.out.println();
+        }
+}
+
+
+class Pattern16{
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        int n = 20;
+        
+        
+       obj.printPattern(n);
+
+        
+        
+    }
+}
+
