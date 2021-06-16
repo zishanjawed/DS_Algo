130c881445a720d013be32e5f50216711f5b1fdf
zishanjawed <zishanexplore@gmail.com>
2021-06-16 21:30:12 +0530

    Pepcoding --> Pattern8

---
+++ Pepcoding/Pattern/Pattern8.java
@@ -0,0 +1,53 @@
+/* 
+1. You are given a number n.
+2. You've to create a pattern of * and separated by tab as shown in output format.
+Input Format
+A number n
+Output Format
+pat81
+Question Video
+
+
+Constraints
+1 <= n <= 100
+Sample Input
+5
+Sample Output
+				*	
+			*		
+		*			
+	*				
+*
+*/
+class Solution {
+    public void printPattern(int n) {
+      
+       
+        for (int i=1; i<=n; i++){
+            
+            for (int j=1; j<=n; j++){
+                
+                if(i + j == n +1){
+                    System.out.print("*\t");
+                }
+                else{
+                    System.out.print("\t");
+                }
+            }
+        
+            System.out.println();
+        }
+
+        
+    }
+}
+
+
+class Pattern8{
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        int n = 20;        
+       obj.printPattern(n);        
+    }
+}
+
