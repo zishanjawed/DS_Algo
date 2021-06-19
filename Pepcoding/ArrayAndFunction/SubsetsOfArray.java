d6a71eecbedfe0da065029384cdf7921663e0c04
zishanjawed <zishanexplore@gmail.com>
2021-06-19 10:40:43 +0530

    Pepcoding --> Array And Function --> Subset

---
+++ Pepcoding/ArrayAndFunction/SubsetsOfArray.java
@@ -0,0 +1,37 @@
+/* 
+1. You are given a number n, representing the count of elements.
+2. You are given n numbers.
+3. You are required to print all subsets of arr. Each subset should be
+on separate line. For more clarity check out sample input and output.
+Input Format
+A number n
+n1
+n2
+.. n number of elements
+Output Format
+[Tab separated elements of subset]
+..
+All subsets
+Question Video
+
+
+Constraints
+1 <= n <= 10
+0 <= n1, n2, .. n elements <= 10^3
+Sample Input
+3
+10
+20
+30
+Sample Output
+-	-	-	
+-	-	30	
+-	20	-	
+-	20	30	
+10	-	-	
+10	-	30	
+10	20	-	
+10	20	30	
+
+*/
+
