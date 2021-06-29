6b6eb3225ee61cf4d745d60965b7336991615efa
zishanjawed <zishanexplore@gmail.com>
2021-06-29 23:22:53 +0530

    PepCoding --> Introduction to Recursiom  --> Power Linear

---
+++ Pepcoding/Introduction to Recursion/Power-linear.java
@@ -0,0 +1,51 @@
+/**
+ *1. You are given a number x.
+2. You are given another number n.
+3. You are required to calculate x raised to the power n. Don't change the signature of power function .
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.
+Input Format
+A number x
+A number n
+Output Format
+x raised to the power n
+Question Video
+
+  COMMENTConstraints
+1 <= x <= 10
+0 <= n <= 9
+Sample Input
+2
+5
+Sample Output
+32
+ */
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn = new Scanner(System.in);
+        
+        int x = scn.nextInt();
+        int n = scn.nextInt();
+         int ans = power(x,n);
+         System.out.println(ans);
+        
+        
+    }
+
+    public static int power(int x, int n){
+        
+      
+        if(n==0)
+          return 1;
+        
+        return x* power(x,n-1);
+        
+    }
+
+}
